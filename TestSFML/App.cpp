5f2c7090e81fe9af007b045ef21edfa04cca3304
Merlette Alexandre <alex.merlette@yahoo.fr>
2022-02-01 11:56:07 +0100

    Init App.cpp+App.h

---
+++ TestSFML/App.cpp
@@ -0,0 +1,19 @@
+#include "App.h"
+
+App::App()
+{
+}
+
+App::~App()
+{
+}
+
+
+bool App::run()
+{
+	return false;
+}
+
+void App::stop()
+{
+}
--- TestSFML/main.cpp
+++ TestSFML/main.cpp
@@ -1,24 +1,14 @@
-#include <SFML/Graphics.hpp>
+#include "App.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    App* app = new App();
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    while(app->run()){};
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    app->stop();
+
+    delete(app);
 
     return 0;
 }

c96c51562e061d020a298ecbbd7a65460ab836af
KorentinB <91191036+KorentinB@users.noreply.github.com>
2022-02-08 10:38:20 +0100

    Add class TestLevel

--- TestSFML/TestLevel.h
+++ TestSFML/TestLevel.h
@@ -14,6 +14,7 @@ class TestLevel
 public:
     TestLevel();
     TestLevel(unsigned long LevelID);
+    ~TestLevel();
 
     void TimeToComplete();
 
@@ -25,49 +26,3 @@ public:
     char* get_DateTest();
     time_t get_TimeToComplete();
 };
-
-TestLevel::TestLevel()
-{
-    m_DateTest = ctime(0);
-}
-
-TestLevel::TestLevel(unsigned long LevelID)
-{
-    m_DateTest = ctime(0);
-    m_LevelID = LevelID;
-}
-
-void TestLevel::TimeToComplete()
-{
-    m_TimeToComplete = ctime(0) - m_DateTest;
-}
-
-void TestLevel::set_CompleteLevel(bool CompleteLevel)
-{
-    m_CompleteLevel = CompleteLevel;
-}
-
-void TestLevel::set_LevelID(unsigned long LevelID)
-{
-    m_LevelID = LevelID;
-}
-
-bool TestLevel::get_CompleteLevel()
-{
-    return m_CompleteLevel;
-}
-
-unsigned long TestLevel::get_LevelID()
-{
-    return m_LevelID;
-}
-
-char* TestLevel::get_DateTest()
-{
-    return m_DT;
-}
-
-time_t TestLevel::get_TimeToComplete()
-{
-    return m_TimeToComplete;
-}

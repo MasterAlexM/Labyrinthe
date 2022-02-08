9b58b2006fb3c899736cd201086b2f3328d8b658
Merlette Alexandre <alex.merlette@yahoo.fr>
2022-02-08 09:49:17 +0100

    Add ClassMenu

--- TestSFML/App.cpp
+++ TestSFML/App.cpp
@@ -1,16 +1,32 @@
 #include "App.h"
 
+
+bool App::_setMenu(Menu* tmpMenu)
+{
+	return false;
+}
+
+Menu* App::_getMenu()
+{
+	return nullptr;
+}
+
 App::App()
 {
+	this->_setMenu(new Menu());
 }
 
 App::~App()
 {
+	delete(this->_getMenu());
 }
 
 
 bool App::run()
 {
+	while (this->_getMenu()->Draw());
+
+
 	return false;
 }
 
--- TestSFML/App.h
+++ TestSFML/App.h
@@ -1,13 +1,18 @@
 #pragma once
+#include "Menu.h"
+
 class App
 {
 private:
-
+	Menu* _menu;
+	bool _setMenu(Menu* tmpMenu);
+	Menu* _getMenu();
 
 public:
 	App();
 	~App();
 	bool run();
 	void stop();
+
 };
 
